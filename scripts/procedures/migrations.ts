19d21587e5dee14c6b628c89b39a37643237a44a
Mariusz Kogen <k0gen@pm.me>
2022-11-23 14:38:57 +0100

    js-main  Please enter the commit message for your changes. Lines starting

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping( {}, "5.17.2.2" );
+    .fromMapping( {}, "5.17.2" );
