3d9e1f35a5d1de54e7675f8925c55a9066222300
Mariusz Kogen <k0gen@pm.me>
2023-09-29 10:58:32 +0200

    Upstream update * version bump * Dockerfile refactor * code cleaning * updated CI to use latest SDK * fixed formatting * licence update * misc changes and updates

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,45 +2,45 @@
 
 set -ea
 
-_term() { 
-  echo "Caught TERM signal!" 
-  kill -TERM "$db_process" 2>/dev/null
-  kill -TERM "$ghost_process" 2>/dev/null
+_term() {
+    echo "Caught TERM signal!"
+    kill -TERM "$db_process" 2>/dev/null
+    kill -TERM "$ghost_process" 2>/dev/null
 }
 
 if [ -d "/run/mysqld" ]; then
-	echo "[i] mysqld already present, skipping creation"
-	chown -R node:node /run/mysqld
+    echo "[i] mysqld already present, skipping creation"
+    chown -R node:node /run/mysqld
 else
-	echo "[i] mysqld not found, creating...."
-	mkdir -p /run/mysqld
-	chown -R node:node /run/mysqld
+    echo "[i] mysqld not found, creating...."
+    mkdir -p /run/mysqld
+    chown -R node:node /run/mysqld
 fi
 
 if [ -d /var/lib/ghost/content/mysql/mysql ]; then
-	echo "[i] MariaDB directory already present, skipping creation"
-	chown -R node:node /var/lib/ghost/content/mysql
+    echo "[i] MariaDB directory already present, skipping creation"
+    chown -R node:node /var/lib/ghost/content/mysql
 else
-	echo "[i] MariaDB data directory not found, creating initial DBs"
+    echo "[i] MariaDB data directory not found, creating initial DBs"
 
-	mkdir -p /var/lib/ghost/content/mysql
+    mkdir -p /var/lib/ghost/content/mysql
     chown -R node:node /var/lib/ghost/content/mysql
 
-	mysql_install_db --user=node --ldata=/var/lib/ghost/content/mysql > /dev/null
+    mysql_install_db --user=node --ldata=/var/lib/ghost/content/mysql >/dev/null
 
-	if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
-		MYSQL_ROOT_PASSWORD=$(cat /dev/urandom | base64 | head -c 16)
-		echo "[i] MariaDB root Password: $MYSQL_ROOT_PASSWORD"
-	fi
+    if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
+        MYSQL_ROOT_PASSWORD=$(cat /dev/urandom | base64 | head -c 16)
+        echo "[i] MariaDB root Password: $MYSQL_ROOT_PASSWORD"
+    fi
 
-	MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
     tfile=$(mktemp)
     if [ ! -f "$tfile" ]; then
         return 1
     fi
 
-    cat << EOF > $tfile
+    cat <<EOF >$tfile
 USE mysql;
 FLUSH PRIVILEGES ;
 GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
@@ -49,8 +49,8 @@ SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
 DROP DATABASE IF EXISTS test ;
 FLUSH PRIVILEGES ;
 EOF
-	mkdir -p /var/lib/ghost/content/start9
-	cat << EOF > /var/lib/ghost/content/start9/stats.yaml
+    mkdir -p /var/lib/ghost/content/start9
+    cat <<EOF >/var/lib/ghost/content/start9/stats.yaml
 data:
   MariaDB root password:
     copyable: true
@@ -61,13 +61,13 @@ data:
     value: $MYSQL_ROOT_PASSWORD
 version: 2
 EOF
- 
-    /usr/sbin/mysqld --user=node --datadir='/var/lib/ghost/content/mysql' --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
+
+    /usr/sbin/mysqld --user=node --datadir='/var/lib/ghost/content/mysql' --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 <$tfile
     rm -f $tfile
 
-	echo
-	echo 'MariaDB init process done. Ready for Ghost.'
-	echo
+    echo
+    echo 'MariaDB init process done. Ready for Ghost.'
+    echo
 fi
 
 /usr/sbin/mysqld --user=node --datadir='/var/lib/ghost/content/mysql' --console --skip-name-resolve --skip-networking=0 &
@@ -77,7 +77,6 @@ TOR_ADDRESS=$(yq e .tor-address /var/lib/ghost/content/start9/config.yaml)
 LAN_ADDRESS=$(echo "$TOR_ADDRESS" | sed -r 's/(.+)\.onion/\1.local/g')
 TOR_ADDR="http://$TOR_ADDRESS"
 LAN_ADDR="https://$LAN_ADDRESS"
-
 
 export url=$TOR_ADDR
 export database__client=mysql
@@ -103,7 +102,7 @@ sed -i 's#https://static.ghost.org/v4.0.0/images/feature-image.jpg#'$LAN_ADDR'/g
 sed -i 's#https://static.ghost.org/v4.0.0/images/ghost-orb-1.png#/ghost/assets/local/ghost-orb-1.png#g' /var/lib/ghost/current/core/built/admin/assets/ghost-*.js
 sed -i 's#https://static.ghost.org/v4.0.0/images/ghost-orb-2.png#/ghost/assets/local/ghost-orb-2.png#g' /var/lib/ghost/current/core/built/admin/assets/ghost-*.js
 
-start-ghost.sh node current/index.js &
+docker-entrypoint.sh node current/index.js &
 ghost_process=$!
 
 trap _term TERM
--- instructions.md
+++ instructions.md
@@ -1,7 +1,9 @@
 # Welcome to Ghost
+
 **Your own blogging platform!**
 
 The moment you start Ghost, it is ready to run and waiting for you to finish some basic site setup in **Ghost Admin** at:
+
 > `http://<your-ghost-address-from-interfaces>/ghost`
 
 Alternatively, you can copy your unique address from the **Services > Ghost > Properties** section.
@@ -10,9 +12,9 @@ After simple site setup and account creation, make sure to store your password s
 
 Further admin registrations are closed. You can now start blogging!
 
->📧 Direct transactional and bulk e-mail delivery is not supported. If you need full e-mail support in Ghost, you have to use [Mailgun](https://ghost.org/docs/faq/mailgun-newsletters/).
+> 📧 Direct transactional and bulk e-mail delivery is not supported. If you need full e-mail support in Ghost, you have to use [Mailgun](https://ghost.org/docs/faq/mailgun-newsletters/).
 
->⚠️ Ghost is expected to work **only on one domain** address. In this case, it is operating on a `.onion` address and that will give you a fully-fledged Ghost experience. For your convenience, a `.local` (LAN) address has been enabled for uploading larger files and making maintenance changes locally (for example, if Tor is overloaded), but be advised that some preview features might not work as expected when working on the LAN address.
+> ⚠️ Ghost is expected to work **only on one domain** address. In this case, it is operating on a `.onion` address and that will give you a fully-fledged Ghost experience. For your convenience, a `.local` (LAN) address has been enabled for uploading larger files and making maintenance changes locally (for example, if Tor is overloaded), but be advised that some preview features might not work as expected when working on the LAN address.
 
 We recommend visiting the [Ghost resources page](https://ghost.org/resources) to learn more about using Ghost.
 
