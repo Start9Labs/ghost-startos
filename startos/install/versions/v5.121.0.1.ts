037c5fb741079f2381317c60e85409ad7e5dbba6
Mariusz Kogen <k0gen@pm.me>
2025-07-29 18:18:00 +0200

    fix undefined

--- startos/install/versions/v5.121.0.1.ts
+++ startos/install/versions/v5.121.0.1.ts
@@ -28,9 +28,10 @@ export const v_5_121_0_1 = VersionInfo.of({
       const urls = await getPrimaryInterfaceUrls(effects)
 
       await storeJson.write(effects, {
-        url: urls.find((u) => u.startsWith('http:') && u.includes('.onion'))!,
+        url:
+          urls.find((u) => u.startsWith('http:') && u.includes('.onion')) || '',
         privacy__useTinfoil: !!configFile?.useTinfoil,
-        database__connection__password: statsFile.data.MariaDB.value,
+        database__connection__password: statsFile?.data?.MariaDB?.value || '',
         smtp: {
           selection: 'disabled',
           value: {},
